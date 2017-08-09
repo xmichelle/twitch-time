a09c128a393fffe22a3e8fab346dc321df4fab10
xu.miche@gmail.com <xu.miche@gmail.com>
2017-08-09 15:18:51 -0700

    Set up Knex to insert and obtain data from the PostgreSQL database. #1

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ const app = express()
 const bodyParser = require('body-parser')
 const path = require('path')
 
+const knex = require('knex')({
+  dialect: 'pg',
+  connection: process.env.DATABASE_URL
+})
+
 const publicPath = path.join(__dirname, 'public')
 const staticMiddleware = express.static(publicPath)
 
@@ -13,34 +18,42 @@ app.use(staticMiddleware)
 app.use(bodyParser.json())
 
 const clientId = process.env.CLIENT_ID
-// const channelQueryUrl = 'https://api.twitch.tv/kraken/search/channels?query=' + '' + '&client_id=' + clientID
-const testChannel = `https://api.twitch.tv/kraken/search/channels?query=nightblue3&client_id=${clientId}`
 
-const channelSearchOptions = {
-  url: testChannel,
-  headers: {
-    'Accept': 'application/vnd.twitchtv.v5+json'
-  },
-  json: true
-}
-
-app.get('/search', (req, res) => {
-  request(channelSearchOptions, (err, response, body) => {
-    if (!err && response.statusCode === 200) {
-      res.json(body)
-    }
-  })
-})
-
-const favoriteChannels = []
+// app.get('/search', (req, res) => {
+//   console.log(req.query)
+//   // req.query will return an object so need to specify the property in order to get the value; temporary property will be named term
+//   const channelSearchOptions = {
+//     url: `https://api.twitch.tv/kraken/search/channels?query=${req.query.term}&client_id=${clientId}`,
+//     headers: {
+//       'Accept': 'application/vnd.twitchtv.v5+json'
+//     },
+//     json: true
+//   }
+//   request(channelSearchOptions, (err, response, body) => {
+//     if (!err && response.statusCode === 200) {
+//       res.json(body)
+//     }
+//   })
+// })
 
 app.get('/favorites', (req, res) => {
-  res.json(favoriteChannels)
+  knex
+    .select('*')
+    .from('streamers')
+    .then((data) => {
+      res.json(data)
+    })
 })
 
 app.post('/favorites', (req, res) => {
-  favoriteChannels.push(req.body)
-  res.sendStatus(201)
+  const channelData = req.body
+  knex
+    .insert(channelData)
+    .into('streamers')
+    .returning('*')
+    .then((data) => {
+      res.status(201).json(data)
+    })
 })
 
 app.listen(process.env.PORT, () => {
